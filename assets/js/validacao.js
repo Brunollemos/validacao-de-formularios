f35c170e2fd1cae3383ed61bd4754ec79fbb8664
Bruno Lemos <brunollemoscontato@gmail.com>
2022-10-08 05:31:23 -0300

    Criando função checaCPFRepetido

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -78,3 +78,27 @@ function validaCPF(input) {
 
     input.setCustomValidity(mensagem);
 }
+
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+    let cpfValido = true;
+
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf) {
+            cpfValido = false;
+        }
+    })
+
+    return cpfValido
+}
